9b234d3a46a8ea2a3ad76f81fa8a8fbc307a0f19
kelvinmayeye <kevmayeye97@gmail.com>
2023-04-14 18:35:51 +0300

    changed post request method

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('ussd',[UssdController::class,'startUssd']);
+Route::post('ussd',[UssdController::class,'startUssd']);
