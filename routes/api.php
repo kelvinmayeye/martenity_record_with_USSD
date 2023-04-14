0274b669379552b7b21147bc677efe5eaf36f4c1
kelvinmayeye <kevmayeye97@gmail.com>
2023-04-14 14:33:42 +0300

    fixed controller not found

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('ussd',[UssdContoller::class,'startUssd']);
+Route::get('ussd',[UssdController::class,'startUssd']);
