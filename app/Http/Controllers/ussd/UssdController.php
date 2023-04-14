83ad5515de4556ebcce738d686c565fa55030f77
kelvinmayeye <kevmayeye97@gmail.com>
2023-04-14 20:24:33 +0300

    use afrikan talk original codes

--- app/Http/Controllers/ussd/UssdController.php
+++ app/Http/Controllers/ussd/UssdController.php
@@ -15,50 +15,29 @@ $serviceCode = $request->serviceCode;
 $phoneNumber = $request->phoneNumber;
 $text = $request->text;
 
-// Parse the user's input
-$menu_items = [
-    1 => [
-        'text' => 'list ya matatizo',
-        'description' => '1.Maumivu ya mgongo\n2.Shinikizo la damu\n3.Kuvimba miguu\n4.Kutoka damu\n99.Rudi menu kuu\n0.Sitisha',
-        'example' => '',
-    ],
-    2 => [
-        'text' => 'Python',
-        'description' => 'Interpreted, high-level, general-purpose programming language.',
-        'example' => 'Web development, scientific computing, data analysis, artificial intelligence (AI), machine learning (ML), automation, and many more.',
-    ],
-    3 => [
-        'text' => 'Java',
-        'description' => 'Object-oriented programming language for general-purpose computing.',
-        'example' => 'Enterprise applications, Android mobile applications, game development, scientific applications, and many more.',
-    ],
-    4 => [
-        'text' => 'JavaScript',
-        'description' => 'High-level, dynamic programming language for creating interactive web pages.',
-        'example' => 'Client-side scripting, server-side scripting, desktop and mobile application development, and many more.',
-    ],
-    5 => [
-        'text' => 'C#',
-        'description' => 'Modern, object-oriented programming language for building Windows applications.',
-        'example' => 'Windows desktop applications, web applications, games, mobile apps, and many more.',
-    ],
-];
-$selected_item = isset($menu_items[$text]) ? $menu_items[$text] : null;
-
-// Determine the response to send
-if ($selected_item) {
-    // This is a second level response where the user selected a programming language option
-    $response = "END " . $selected_item['text'] . "\n\n";
-    $response .= $selected_item['description'] . "\n\n";
-    $response .= "Examples of applications it can build: " . $selected_item['example'];
-} else {
+if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON Choose a programming language: \n";
-    $response .= "1. Ripoti Tatizo\n";
-    $response .= "2. Angalia Jibu la Ripoti\n";
-    $response .= "3. Hakiki Taarifa\n";
-    $response .= "4. Pata ufipisho wa Ripoti\n";
-    $response .= "5. Sitisha";
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
+
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
+
+} else if ($text == "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+
+} else if($text == "1*1") {
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber  = "ACC1001";
+
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your account number is ".$accountNumber;
+
 }
 
 // Send the response back to the API
