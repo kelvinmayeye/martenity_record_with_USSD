d536e3dff0e2ffba3d5bd42a6d4d99c7a26213c9
kelvinmayeye <kevmayeye97@gmail.com>
2023-04-15 15:36:38 +0300

    interacting USSD with Database

--- app/Http/Controllers/ussd/UssdController.php
+++ app/Http/Controllers/ussd/UssdController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\ussd;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\pregnantreports\PregnantReport;
 
 class UssdController extends Controller{
     public function startUssd(Request $request){
